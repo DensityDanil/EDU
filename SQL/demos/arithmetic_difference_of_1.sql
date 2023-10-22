6c0b37c1e11717c0d5a887aeb8b550e0aca8c018
Bristol Zoo <danpodlesniy@gmail.com>
2023-10-22 19:57:21 +0300

    sql - update case for two arithmetic diff-s in seria

--- SQL/demos/arithmetic_difference_of_1.sql
+++ SQL/demos/arithmetic_difference_of_1.sql
@@ -4,16 +4,27 @@ with tmp as (
 select 1 as rn union all
 select 2 as rn union all
 select 3 as rn union all
-select 5 as rn 
+
+
+select 5 as rn union all
+select 6 as rn union all
+
+select 8 as rn union all
+select 10 as rn
 )
 
-select a.*
+,match_arith_progg as
+( select a.*
 from tmp a left join tmp b on (b.rn-a.rn)=1
 where a.rn IS NOT NULL AND b.rn IS NOT NULL
 union distinct 
 select b.*
 from tmp a left join tmp b on (b.rn-a.rn)=1
 where a.rn IS NOT NULL AND b.rn IS NOT NULL
+)
+
+select *
+from match_arith_progg
 
 -- output
 -- 2
